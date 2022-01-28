977bc74575d79acef1a595b51466424fe63d1d4f
blessedjasonmwanza <mwanzabj@gmail.com>
2022-01-28 16:45:45 +0200

    Improve UI to match design

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -20,13 +20,15 @@ export default function AddBook() {
   return (
     <form className="add-book-form" onSubmit={(e) => submitBookToStore(e)}>
       <h2>ADD NEW BOOK</h2>
-      <input name="book_title" placeholder="Book title" className="input-title" required />
-      <select name="book_category" className="input-category" required>
-        <option defaultValue value="">Category</option>
-        <option value="category A">Sample category A</option>
-        <option value="category B">Sample category B</option>
-      </select>
-      <button type="submit" className="add-book">ADD BOOK</button>
+      <div className="add-inputs">
+        <input name="book_title" placeholder="Book title" className="input-title" required />
+        <select name="book_category" className="input-category" required>
+          <option defaultValue value="">Category</option>
+          <option value="category A">Sample category A</option>
+          <option value="category B">Sample category B</option>
+        </select>
+        <button type="submit" className="add-book">ADD BOOK</button>
+      </div>
     </form>
   );
 }
